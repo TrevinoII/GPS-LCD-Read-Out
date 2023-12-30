6623a01f2ef9e38e8cac5300fd21111a03995b96
cyber_cadet <99143418+TrevinoII@users.noreply.github.com>
2023-12-30 01:11:28 -0600

    Update GPSReadOut.ino

---
+++ GPSReadOut.ino
@@ -0,0 +1,9 @@
+#include <TinyGPSPlus.h>
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+
+
+// setup and init lcd
+const int rs=2, en=3, d4=4, d5=5, d6=6, d7=7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
